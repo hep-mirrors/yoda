ca48b114d38ec6af4980f3acc70f4e015358b595
buckley <none@none>
2008-03-13 11:42:42 +0000

    Bin class reviewed

--- include/YODA/Bin.h
+++ include/YODA/Bin.h
@@ -1,61 +1,106 @@
 namespace YODA {
 
-// bin in a 1d histogram
-class Bin {
+  /// @brief A Bin in a 1D histogram
+  /// Lower edge is inclusive.
+  class Bin {
 
-public:
+  public:
 
-  // constructor giving  limits
-  Bin (pair<double,double> limits);
+    /// @name Constructor giving bin low and high edges.
+    //@{
+    Bin (double lowedge, double highedge);
+    Bin (pair<double,double> edges);
+    //@}
 
-  // default destructor
-  virtual ~Bin ();
+  public:
 
-public:
+    /// @brief Fill this bin with weight @a weight.
+    /// @todo Be careful about negative weights.
+    void fill(double coord, double weight=1.0);
 
-  void fill (double weight = 1.0);
+  public:
 
-public:
+    /// @name X-axis info
+    //@{
+    /// Lower limit of the bin (inclusive).
+    double lowEdge() const;
 
-  pair<double,double> limits () const;
+    /// Upper limit of the bin (exclusive).
+    double highEdge() const;
 
-  size_t numEntries () const;
+    /// Get the {low,high} edges as an STL @c pair.
+    pair<double,double> edges() const;
 
-  double sumWeights () const;
+    /// Separation of low and high edges, i.e. high-low.
+    double width() const;
 
-  double sumSquaredWeights () const;
+    /// The mean position in the bin.
+    double focus() const;
 
-  size_t nanEntries () const;
+    /// Geometric centre of the bin, i.e. high+low/2.0
+    double midpoint() const;
+    //@}
 
-public:
+    /// @name Bin content info
+    //@{
+    /// The area is the sum of weights in the bin, i.e. the
+    /// width of the bin has no influence on this figure.
+    double area() const;
 
-  // convert bin to datapoint assuming some statistics
-  // interpretation
-  template<class Statistics>
-  DataPoint convert ();
+    /// The height is defined as area/width.
+    double height() const;
+    //@}
 
-public:
+    /// @name Error info
+    //@{
+    /// Error computed using binomial statistics on the sum of bin weights,
+    /// i.e. err_area = sqrt{sum{weights}}
+    double areaError() const;
 
-  // add two bins
-  Bin operator += (const Bin&);
+    /// As for the height vs. area, the height error includes a scaling factor
+    /// of the bin width, i.e. err_height = sqrt{sum{weights}} / width.
+    double heightError() const;
 
-private:
+    /// The x error is the standard error on the bin focus. 
+    double xError() const;
 
-  // default constructor is private
-  // there's no bin without limits specified
-  Bin ();
+    /// @todo RMS?
+    //@}
 
-  pair<double,double> _limits;
+  private:
 
-  size_t _numEntries;
+    double sumWeights () const;
 
-  double _sumWeights;
+    double sumSquaredWeights () const;
 
-  double _sumSquaredWeights;
+  public:
 
-  size_t _nanEntries;
+    // convert bin to datapoint assuming some statistics
+    // interpretation
+    //template<class Statistics>
+    //DataPoint convert ();
 
-};
+  private:
+
+    /// Add two bins (for use by Histo1D).
+    Bin operator += (const Bin&);
+    Bin operator -= (const Bin&);
+
+  private:
+
+    pair<double,double> _limits;
+
+    size_t _numEntries;
+
+    double _sumWeight;
+
+    double _sumWeight2;
+
+    double _sumXWeight;
+
+    double _sumX2Weight;
+
+  };
 
 }
 
