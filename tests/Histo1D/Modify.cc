8d042ab8ee0d858d7337bfb91a05f6acb90a1ec3
mkawalec <none@none>
2011-08-24 11:03:23 +0000

    Added some rudimentary bin modification checking.

---
+++ tests/Histo1D/Modify.cc
@@ -0,0 +1,30 @@
+#include "YODA/Histo1D.h"
+
+#include <iostream>
+using namespace std;
+using namespace YODA;
+
+int main() {
+  
+  Histo1D h(100, 0, 100);
+  cout << "Trying to merge bins:                    ";
+  h.mergeBins(0, 10);
+  cout << "PASS" << endl;
+
+  cout << "Testing if bin nuber was updated:        ";
+  if(h.numBins() != 90) {
+    cout << "FAIL" << endl;
+    return -1;
+  }
+  cout << "PASS" << endl;
+  
+  cout << "Checking if bin size was updated:        ";
+  if(!fuzzyEquals(h.bin(0).xMin(), 0) || !fuzzyEquals(h.bin(0).xMax(),11)){
+    cout << "FAIL" << endl;
+    return -1;
+  }
+  cout << "PASS" << endl;
+
+
+  return EXIT_SUCCESS;
+}
