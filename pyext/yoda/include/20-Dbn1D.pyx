46d0c62f15644e4240067a1083f68f65f81a2ef6
buckley <none@none>
2011-12-07 22:06:04 +0000

    Mapping the Dbn1D and Dbn2D classes into Python.

--- pyext/yoda/include/20-Dbn1D.pyx
+++ pyext/yoda/include/20-Dbn1D.pyx
@@ -1,6 +1,28 @@
 cdef extern from "YODA/Dbn1D.h" namespace "YODA":
     cdef cppclass cDbn1D "Dbn1D":
-        pass
+        cDbn1D()
+        cDbn1D(unsigned int numEntries, double sumW, double sumW2, double sumWX, double sumWX2)
+        void fill(double val, double weight)
+        void reset()
+        void scaleW(double scalefactor)
+        void scaleX(double factor)
+        double mean()
+        double xMean()
+        double variance()
+        double xVariance()
+        double stdDev()
+        double xStdDev()
+        double stdErr()
+        double xStdErr()
+        double rms()
+        double xRMS()
+        unsigned long numEntries()
+        double effNumEntries()
+        double sumW()
+        double sumW2()
+        double sumWX()
+        double sumWX2()
+        # TODO: map operator+ and operator-, both internal and external
 
 
 cdef class Dbn1D:
@@ -14,6 +36,16 @@ cdef class Dbn1D:
         if self._dealloc:
             del self.thisptr
 
+
+    def __init__(self, *args, **kwargs):
+        cdef:
+            unsigned int numEntries
+            double sumW, sumW2, sumWX, sumWX2
+        assert len(args) == 5
+        numEntries, sumW, sumW2, sumWX, sumWX2 = args
+        self.setptr(new cDbn1D(numEntries, sumW, sumW2, sumWX, sumWX2), True)
+
+
     cdef Dbn1D setptr(self, cDbn1D *ptr, bool dealloc):
         if self._dealloc:
             del self.thisptr
@@ -24,6 +56,70 @@ cdef class Dbn1D:
 
     cdef cDbn1D* ptr(self):
         return self.thisptr
+
+
+    def fill(self, double x, double weight=1.0):
+        self.ptr().fill(x, weight)
+
+    def reset(self):
+        self.ptr().reset()
+
+    def scaleW(self, scalefactor):
+        self.ptr().scaleW(scalefactor)
+
+    def scaleX(self, factor):
+        self.ptr().scaleX(factor)
+
+    def mean(self):
+        return self.ptr().mean()
+
+    def xMean(self):
+        return self.ptr().xMean()
+
+    def variance(self):
+        return self.ptr().variance()
+
+    def xVariance(self):
+        return self.ptr().xVariance()
+
+    def stdDev(self):
+        return self.ptr().stdDev()
+
+    def xStdDev(self):
+        return self.ptr().xStdDev()
+
+    def stdErr(self):
+        return self.ptr().stdDev()
+
+    def xStdErr(self):
+        return self.ptr().xStdDev()
+
+    def rms(self):
+        return self.ptr().rms()
+
+    def xRMS(self):
+        return self.ptr().xRMS()
+
+    def numEntries(self):
+        return self.ptr().numEntries()
+
+    def effNumEntries(self):
+        return self.ptr().effNumEntries()
+
+    def sumW(self):
+        return self.ptr().sumW()
+
+    def sumW2(self):
+        return self.ptr().sumW2()
+
+    def sumWX(self):
+        return self.ptr().sumWX()
+
+    def sumWX2(self):
+        return self.ptr().sumWX2()
+
+    # TODO: map operator+ and operator-, both internal and external
+
 
 
 cdef Dbn1D Dbn1D_fromptr(cDbn1D *ptr, dealloc=False):
--- pyext/yoda/include/20-Dbn2D.pyx
+++ pyext/yoda/include/20-Dbn2D.pyx
@@ -1,6 +1,33 @@
 cdef extern from "YODA/Dbn2D.h" namespace "YODA":
     cdef cppclass cDbn2D "Dbn2D":
-        pass
+        cDbn2D()
+        cDbn2D(unsigned int numEntries, double sumW, double sumW2, double sumWX, double sumWX2, double sumWY, double sumWY2, double sumWXY)
+        void fill(double valX, double valY, double weight)
+        void reset()
+        void scaleW(double scalefactor)
+        void scaleX(double factor)
+        void scaleY(double factor)
+        void scaleXY(double factor)
+        double xMean()
+        double yMean()
+        double xVariance()
+        double yVariance()
+        double xStdDev()
+        double yStdDev()
+        double xStdErr()
+        double yStdErr()
+        double yRMS()
+        double xRMS()
+        unsigned long numEntries()
+        double effNumEntries()
+        double sumW()
+        double sumW2()
+        double sumWX()
+        double sumWX2()
+        double sumWY()
+        double sumWY2()
+        double sumWXY()
+        # TODO: map operator+ and operator-, both internal and external
 
 
 cdef class Dbn2D:
@@ -14,6 +41,16 @@ cdef class Dbn2D:
         if self._dealloc:
             del self.thisptr
 
+
+    def __init__(self, *args, **kwargs):
+        cdef:
+            unsigned int numEntries
+            double sumW, sumW2, sumWX, sumWX2, sumWY, sumWY2, sumWXY
+        assert len(args) == 8
+        numEntries, sumW, sumW2, sumWX, sumWX2, sumWY, sumWY2, sumWXY = args
+        self.setptr(new cDbn2D(numEntries, sumW, sumW2, sumWX, sumWX2, sumWY, sumWY2, sumWXY), True)
+
+
     cdef Dbn2D setptr(self, cDbn2D *ptr, bool dealloc):
         if self._dealloc:
             del self.thisptr
@@ -24,6 +61,82 @@ cdef class Dbn2D:
 
     cdef cDbn2D* ptr(self):
         return self.thisptr
+
+
+    def fill(self, double x, double y, double weight=1.0):
+        self.ptr().fill(x, y, weight)
+
+    def reset(self):
+        self.ptr().reset()
+
+    def scaleW(self, scalefactor):
+        self.ptr().scaleW(scalefactor)
+
+    def scaleX(self, factor):
+        self.ptr().scaleX(factor)
+
+    def scaleY(self, factor):
+        self.ptr().scaleY(factor)
+
+    def xMean(self):
+        return self.ptr().xMean()
+
+    def yMean(self):
+        return self.ptr().yMean()
+
+    def xVariance(self):
+        return self.ptr().xVariance()
+
+    def yVariance(self):
+        return self.ptr().yVariance()
+
+    def xStdDev(self):
+        return self.ptr().xStdDev()
+
+    def yStdDev(self):
+        return self.ptr().yStdDev()
+
+    def xStdErr(self):
+        return self.ptr().xStdDev()
+
+    def yStdErr(self):
+        return self.ptr().yStdDev()
+
+    def xRMS(self):
+        return self.ptr().xRMS()
+
+    def yRMS(self):
+        return self.ptr().yRMS()
+
+    def numEntries(self):
+        return self.ptr().numEntries()
+
+    def effNumEntries(self):
+        return self.ptr().effNumEntries()
+
+    def sumW(self):
+        return self.ptr().sumW()
+
+    def sumW2(self):
+        return self.ptr().sumW2()
+
+    def sumWX(self):
+        return self.ptr().sumWX()
+
+    def sumWX2(self):
+        return self.ptr().sumWX2()
+
+    def sumWY(self):
+        return self.ptr().sumWY()
+
+    def sumWY2(self):
+        return self.ptr().sumWY2()
+
+    def sumWXY(self):
+        return self.ptr().sumWXY()
+
+    # TODO: map operator+ and operator-, both internal and external
+
 
 
 cdef Dbn2D Dbn2D_fromptr(cDbn2D *ptr, dealloc=False):
