a0d7c432f310922ea4eb25775743b2311e0ef378
buckley <none@none>
2012-07-11 16:52:06 +0000

    Adding a Plot class, designed solely to make it easy to access BEGIN YODA_PLOT ... END YODA_PLOT sections (which are purely collections of styling annotations) in YODA data files.

---
+++ include/YODA/Plot.h
@@ -0,0 +1,33 @@
+// -*- C++ -*-
+//
+// This file is part of YODA -- Yet more Objects for Data Analysis
+// Copyright (C) 2008-2012 The YODA collaboration (see AUTHORS for details)
+//
+#ifndef YODA_Plot_h
+#define YODA_Plot_h
+
+#include "YODA/AnalysisObject.h"
+
+namespace YODA {
+
+
+  /// A plot object: really just a handler for plot style annotations in data files.
+  class Plot : public AnalysisObject {
+  public:
+
+    /// @name Constructors
+    //@{
+
+    /// Default constructor
+    Histo1D()
+      : AnalysisObject("Plot", "", "")
+    { }
+
+    //@}
+
+  };
+
+
+}
+
+#endif
