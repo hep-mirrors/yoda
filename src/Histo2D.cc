a9eda78c5a09c282250a3d609b29de60f11a0a54
buckley <none@none>
2013-03-04 15:08:32 +0000

    Copyright date update!

--- src/Histo2D.cc
+++ src/Histo2D.cc
@@ -1,7 +1,7 @@
 // -*- C++ -*-
 //
 // This file is part of YODA -- Yet more Objects for Data Analysis
-// Copyright (C) 2008-2012 The YODA collaboration (see AUTHORS for details)
+// Copyright (C) 2008-2013 The YODA collaboration (see AUTHORS for details)
 //
 #include "YODA/Histo2D.h"
 #include "YODA/Scatter3D.h"
--- src/ReaderAIDA.cc
+++ src/ReaderAIDA.cc
@@ -1,7 +1,7 @@
 // -*- C++ -*-
 //
 // This file is part of YODA -- Yet more Objects for Data Analysis
-// Copyright (C) 2008-2012 The YODA collaboration (see AUTHORS for details)
+// Copyright (C) 2008-2013 The YODA collaboration (see AUTHORS for details)
 //
 #include "YODA/ReaderAIDA.h"
 #include "YODA/Utils/StringUtils.h"
