2cd053cae1c720b7e361a6d576583af0538097f2
Dave Mallows <dave.mallows@gmail.com>
2013-06-02 10:44:52 +0100

    Added Python module for ROOT conversion

---
+++ pyext/yoda/pyroot_helpers.hh
@@ -0,0 +1,9 @@
+#include "YODA/ROOTCnv.h"
+#include "Python.h"
+#include "YODA/Profile1D.h"
+#include "TPython.h"
+
+// One useful shim to help us
+inline PyObject* root_to_py_owned(TObject* root_obj) {
+  return TPython::ObjectProxy_FromVoidPtr(root_obj, root_obj->ClassName(), kFALSE);
+}
