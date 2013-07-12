706be239900f5090c9ca555152efd51fe86e1f84
Andy Buckley <andy@insectnation.org>
2013-07-12 12:20:04 +0200

    Pull yoda.rootcompat into the default import namespace if possible

--- pyext/yoda/__init__.py
+++ pyext/yoda/__init__.py
@@ -1,1 +1,8 @@
+## Pull in core YODA C++/Python extension functionality
 from yoda.core import *
+
+## Try to pull in optional ROOT compatibility
+try:
+    from yoda.rootcompat import *
+except:
+    pass
