aa9e554c7a460c6870b9abb80439f4835fc32d67
Andy Buckley <andy@insectnation.org>
2013-07-12 12:17:24 +0200

    Adding ROOT detection in configure and otherwise updating Dave's rootcompat module so that it'll compile. There might be an inconvenient ROOT version dependency in the signature of one of the PyROOT API functions that is used as a shim :-(

--- pyext/yoda/pyroot_helpers.hh
+++ pyext/yoda/pyroot_helpers.hh
@@ -5,5 +5,7 @@
 
 // One useful shim to help us
 inline PyObject* root_to_py_owned(TObject* root_obj) {
-  return TPython::ObjectProxy_FromVoidPtr(root_obj, root_obj->ClassName(), kFALSE);
+  return TPython::ObjectProxy_FromVoidPtr(root_obj, root_obj->ClassName());
+  /// @todo Different signatures in different ROOT versions?
+  //return TPython::ObjectProxy_FromVoidPtr(root_obj, root_obj->ClassName(), kFALSE);
 }
