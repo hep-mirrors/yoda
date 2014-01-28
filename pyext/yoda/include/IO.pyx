d97a88ad804515bf57eb26bf5a551ea810b2e7d1
Andy Buckley <andy@insectnation.org>
2014-01-28 17:11:36 +0100

    Clean-ups and API improvements in Python IO functions.

--- pyext/yoda/include/IO.pyx
+++ pyext/yoda/include/IO.pyx
@@ -9,31 +9,70 @@ potential memory limits.
 
 import sys
 
-cdef list _aobjects_to_list(vector[c.AnalysisObject*] *aobjects):
+## Make a Python list of analysis objects from a C++ vector of them
+cdef list _aobjects_to_list(vector[c.AnalysisObject*]* aobjects):
     cdef list out = []
-    cdef c.AnalysisObject *ao
+    cdef c.AnalysisObject* ao
     cdef size_t i
-    for i in range(aobjects.size()):
+    for i in xrange(aobjects.size()):
         ao = deref(aobjects)[i]
         out.append(
             util.new_owned_cls(
                 globals()[ao.type().c_str()], ao))
     return out
 
-# Set a istringstream's string from a C/Python string
-cdef void _make_iss(c.istringstream &iss, char *s):
+## Make a Python dict of analysis objects from a C++ vector of them
+cdef dict _aobjects_to_dict(vector[c.AnalysisObject*]* aobjects):
+    cdef dict out = {}
+    cdef c.AnalysisObject* ao
+    cdef size_t i
+    for i in xrange(aobjects.size()):
+        ao = deref(aobjects)[i]
+        newao = util.new_owned_cls( globals()[ao.type().c_str()], ao )
+        out[newao.path] = newao
+    return out
+
+## Set a istringstream's string from a C/Python string
+cdef void _make_iss(c.istringstream &iss, char* s):
     iss.str(string(s))
+
+## Read a file's contents as a returned string
+## The file argument can either be a file object, filename, or special "-" reference to stdin
+def _str_from_file(file_or_filename):
+    if hasattr(file_or_filename, 'read'):
+        s = file_or_filename.read()
+    elif file_or_filename == "-":
+        s = sys.stdin.read()
+    else:
+        f = open(file_or_filename)
+        s = f.read()
+        f.close()
+    return s
+
+## Write a string to a file
+## The file argument can either be a file object, filename, or special "-" reference to stdout
+def _str_to_file(s, file_or_filename):
+    if hasattr(file_or_filename, 'write'):
+        file_or_filename.write(s)
+    elif file_or_filename == "-":
+        sys.stdout.write(s)
+    else:
+        f = open(file_or_filename, "w")
+        s = f.write(s)
+        f.close()
+
 
 
 ##
 ## Readers
 ##
 
-def read(filename):
+def read(filename, asdict=True):
     """
-    Read data objects from the provided filename,
-    auto-determining the format from the file extension.
-    Returns a list of analysis objects
+    Read data objects from the provided filename, auto-determining the format
+    from the file extension.
+
+    Returns a dict or list of analysis objects depending on the asdict argument.
     """
     cdef c.istringstream iss
     cdef vector[c.AnalysisObject*] aobjects
@@ -42,74 +81,49 @@ def read(filename):
     f.close()
     _make_iss(iss, s)
     c.Reader_create(filename).read(iss, aobjects)
-    # Not as expensive as it looks!
-    return _aobjects_to_list(&aobjects)
+    return _aobjects_to_dict(&aobjects) if asdict else _aobjects_to_list(&aobjects)
 
 
-def readYODA(file_or_filename):
+def readYODA(file_or_filename, asdict=True):
     """
     Read data objects from the provided YODA-format file.
-    Returns a list of analysis objects
+
+    Returns a dict or list of analysis objects depending on the asdict argument.
     """
     cdef c.istringstream iss
     cdef vector[c.AnalysisObject*] aobjects
-
-    if hasattr(file_or_filename, 'read'):
-        s = file_or_filename.read()
-    else:
-        f = open(file_or_filename)
-        s = f.read()
-        f.close()
-
+    s = _str_from_file(file_or_filename)
     _make_iss(iss, s)
     c.ReaderYODA_create().read(iss, aobjects)
-
-    # Not as expensive as it looks!
-    return _aobjects_to_list(&aobjects)
+    return _aobjects_to_dict(&aobjects) if asdict else _aobjects_to_list(&aobjects)
 
 
-def readFLAT(file_or_filename):
+def readFLAT(file_or_filename, asdict=True):
     """
     Read data objects from the provided FLAT-format file.
-    Returns a list of analysis objects
+
+    Returns a dict or list of analysis objects depending on the asdict argument.
     """
     cdef c.istringstream iss
     cdef vector[c.AnalysisObject*] aobjects
-
-    if hasattr(file_or_filename, 'read'):
-        s = file_or_filename.read()
-    else:
-        f = open(file_or_filename)
-        s = f.read()
-        f.close()
-
+    s = _str_from_file(file_or_filename)
     _make_iss(iss, s)
     c.ReaderFLAT_create().read(iss, aobjects)
-
-    # Not as expensive as it looks!
-    return _aobjects_to_list(&aobjects)
+    return _aobjects_to_dict(&aobjects) if asdict else _aobjects_to_list(&aobjects)
 
 
-def readAIDA(file_or_filename):
+def readAIDA(file_or_filename, asdict=True):
     """
     Read data objects from the provided AIDA-format file.
-    Returns a list of analysis objects
+
+    Returns a dict or list of analysis objects depending on the asdict argument.
     """
     cdef c.istringstream iss
     cdef vector[c.AnalysisObject*] aobjects
-
-    if hasattr(file_or_filename, 'read'):
-        s = file_or_filename.read()
-    else:
-        f = open(file_or_filename)
-        s = f.read()
-        f.close()
-
+    s = _str_from_file(file_or_filename)
     _make_iss(iss, s)
     c.ReaderAIDA_create().read(iss, aobjects)
-
-    # Not as expensive as it looks!
-    return _aobjects_to_list(&aobjects)
+    return _aobjects_to_dict(&aobjects) if asdict else _aobjects_to_list(&aobjects)
 
 
 ##
@@ -125,22 +139,12 @@ def write(ana_objs, filename):
     cdef vector[c.AnalysisObject*] vec
     cdef AnalysisObject a
 
-    try:
-        for a in ana_objs:
-            vec.push_back(a._AnalysisObject())
-    except:
-        a = ana_objs
+    aolist = ana_objs.values() if hasattr(ana_objs, "values") else ana_objs if hasattr(ana_objs, "__iter__") else [ana_objs]
+    for a in aolist:
         vec.push_back(a._AnalysisObject())
 
-    # Most of the time we just won't care about memory
     c.Writer_create(filename).write(oss, vec)
-
-    if filename == "-":
-        sys.stdout.write(oss.str().c_str())
-    else:
-        f = open(filename, "w")
-        s = f.write(oss.str().c_str())
-        f.close()
+    _str_to_file(oss.str().c_str(), filename)
 
 
 def writeYODA(ana_objs, file_or_filename):
@@ -151,26 +155,12 @@ def writeYODA(ana_objs, file_or_filename):
     cdef vector[c.AnalysisObject*] vec
     cdef AnalysisObject a
 
-    try:
-        for a in ana_objs:
-            vec.push_back(a._AnalysisObject())
-    except:
-        a = ana_objs
+    aolist = ana_objs.values() if hasattr(ana_objs, "values") else ana_objs if hasattr(ana_objs, "__iter__") else [ana_objs]
+    for a in aolist:
         vec.push_back(a._AnalysisObject())
 
-    # Most of the time we just won't care about memory
     c.WriterYODA_create().write(oss, vec)
-
-    if hasattr(file_or_filename, 'write'):
-        file_or_filename.write(oss.str().c_str())
-    else:
-        fname = file_or_filename
-        if fname == "-":
-            sys.stdout.write(oss.str().c_str())
-        else:
-            f = open(fname, "w")
-            s = f.write(oss.str().c_str())
-            f.close()
+    _str_to_file(oss.str().c_str(), file_or_filename)
 
 
 def writeFLAT(ana_objs, file_or_filename):
@@ -181,26 +171,12 @@ def writeFLAT(ana_objs, file_or_filename):
     cdef vector[c.AnalysisObject*] vec
     cdef AnalysisObject a
 
-    try:
-        for a in ana_objs:
-            vec.push_back(a._AnalysisObject())
-    except:
-        a = ana_objs
+    aolist = ana_objs.values() if hasattr(ana_objs, "values") else ana_objs if hasattr(ana_objs, "__iter__") else [ana_objs]
+    for a in aolist:
         vec.push_back(a._AnalysisObject())
 
-    # Most of the time we just won't care about memory
     c.WriterFLAT_create().write(oss, vec)
-
-    if hasattr(file_or_filename, 'write'):
-        file_or_filename.write(oss.str().c_str())
-    else:
-        fname = file_or_filename
-        if fname == "-":
-            sys.stdout.write(oss.str().c_str())
-        else:
-            f = open(fname, "w")
-            s = f.write(oss.str().c_str())
-            f.close()
+    _str_to_file(oss.str().c_str(), file_or_filename)
 
 
 def writeAIDA(ana_objs, file_or_filename):
@@ -211,23 +187,9 @@ def writeAIDA(ana_objs, file_or_filename):
     cdef vector[c.AnalysisObject*] vec
     cdef AnalysisObject a
 
-    try:
-        for a in ana_objs:
-            vec.push_back(a._AnalysisObject())
-    except:
-        a = ana_objs
+    aolist = ana_objs.values() if hasattr(ana_objs, "values") else ana_objs if hasattr(ana_objs, "__iter__") else [ana_objs]
+    for a in aolist:
         vec.push_back(a._AnalysisObject())
 
-    # Most of the time we just won't care about memory
     c.WriterAIDA_create().write(oss, vec)
-
-    if hasattr(file_or_filename, 'write'):
-        file_or_filename.write(oss.str().c_str())
-    else:
-        fname = file_or_filename
-        if fname == "-":
-            sys.stdout.write(oss.str().c_str())
-        else:
-            f = open(fname, "w")
-            s = f.write(oss.str().c_str())
-            f.close()
+    _str_to_file(oss.str().c_str(), file_or_filename)
