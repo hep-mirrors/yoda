d961c964996f1100a4c56e2d77aad03c177ccb6c
Andy Buckley <andy@insectnation.org>
2014-09-03 17:15:37 +0100

    Add bin searcher test for binnings starting below 0

--- tests/TestBinSearcher.cc
+++ tests/TestBinSearcher.cc
@@ -36,5 +36,17 @@ int main() {
   TESTBS(bs2, 101, 11);
   TESTBS(bs2, 102, 11);
 
+  const vector<double> linedges2 = linspace(10, -1, 101);
+  cout << "Lin edges starting below 0: "; BOOST_FOREACH(double x, logedges) cout << x << " "; cout << endl;
+  YODA::Utils::BinSearcher bs3(linedges2);
+  TESTBS(bs3, -1-1e-5, 0);
+  TESTBS(bs3, -1, 1);
+  TESTBS(bs3, 0, 1);
+  TESTBS(bs3, 1, 1);
+  TESTBS(bs3, 2, 1);
+  TESTBS(bs3, 100, 10);
+  TESTBS(bs3, 101, 11);
+  TESTBS(bs3, 102, 11);
+
   return rtn;
 }
