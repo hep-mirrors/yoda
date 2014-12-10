c91c9510e0cb47ec3f3914ef1556e46f948173d8
Andy Buckley <andy@insectnation.org>
2014-12-10 16:24:22 +0000

    Move plotting.py into the yoda Python package

--- pyext/yoda/__init__.py
+++ pyext/yoda/__init__.py
@@ -1,6 +1,9 @@
 ## Pull in core YODA C++/Python extension functionality
 from yoda.core import *
 
+## Try to pull in plotting tools
+from yoda.plotting import plothist, plothists
+
 ## Try to pull in optional ROOT compatibility
 try:
     from yoda.rootcompat import *
