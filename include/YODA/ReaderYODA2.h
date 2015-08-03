23867cdc5e5704a9adde3ccd02cf2df66dfd0388
Andy Buckley <andy@insectnation.org>
2015-08-03 19:25:04 +0100

    Cosmetic tweak

--- include/YODA/ReaderYODA2.h
+++ include/YODA/ReaderYODA2.h
@@ -12,7 +12,7 @@
 namespace YODA {
 
 
-  /// Persistency reader from YODA flat text data format.
+  /// Persistency reader from YODA text data format.
   class ReaderYODA : public Reader {
   public:
 
