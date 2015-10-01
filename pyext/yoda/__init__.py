67cd73e3663b8673b8fc509282405a5374f26534
Andy Buckley <andy@insectnation.org>
2015-10-01 22:39:51 +0100

    Expose the yoda.plot() Python function in a way that doesn't automatically induce a dependence on matplotlib.

--- pyext/yoda/__init__.py
+++ pyext/yoda/__init__.py
@@ -1,8 +1,10 @@
 ## Pull in core YODA C++/Python extension functionality
 from yoda.core import *
 
-## Try to pull in plotting tools
-from yoda.plotting import plot #, mplot
+## Try to pull in plotting tools in a non-dependency-inducing way
+def plot(*args, **kwargs):
+    from yoda.plotting import plot as p
+    return p(*args, **kwargs)
 
 ## Try to pull in optional ROOT compatibility
 try:
