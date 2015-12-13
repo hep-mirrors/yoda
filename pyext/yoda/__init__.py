ab66195ce4dd3ec06cc793208f9cfbef274ef4f8
Andy Buckley <andy@insectnation.org>
2015-12-13 14:08:17 +0000

    Add a match_aos function to Python, for filtering AO lists/dicts on path patterns and anti-patterns.

--- pyext/yoda/__init__.py
+++ pyext/yoda/__init__.py
@@ -3,10 +3,16 @@ from yoda.core import *
 
 __version__ = core.version()
 
+
+## Pull in useful tools from submodules
+from yoda.search import match_aos
+
+
 ## Try to pull in plotting tools in a non-dependency-inducing way
 def plot(*args, **kwargs):
     from yoda.plotting import plot as p
     return p(*args, **kwargs)
+
 
 ## Try to pull in optional ROOT compatibility
 try:
